3073d20874f1b97d797915a7d23a0842ad066423
Juan Carlos Castillo <juan@accounttech.com>
2022-11-07 21:16:05 -0500

    loggin

--- src/app/modules/auth/components/Login.tsx
+++ src/app/modules/auth/components/Login.tsx
@@ -64,63 +64,20 @@ export function Login() {
     >
       {/* begin::Heading */}
       <div className='text-center mb-11'>
-        <h1 className='text-dark fw-bolder mb-3'>Sign In</h1>
-        <div className='text-gray-500 fw-semibold fs-6'>Your Social Campaigns</div>
+        <h1 className='text-dark fw-bolder mb-3'>Iniciar sesión</h1>
+        {/* <div className='text-gray-500 fw-semibold fs-6'>Your Social Campaigns</div> */}
       </div>
       {/* begin::Heading */}
 
       {/* begin::Login options */}
-      <div className='row g-3 mb-9'>
-        {/* begin::Col */}
-        <div className='col-md-6'>
-          {/* begin::Google link */}
-          <a
-            href='#'
-            className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100'
-          >
-            <img
-              alt='Logo'
-              src={toAbsoluteUrl('/media/svg/brand-logos/google-icon.svg')}
-              className='h-15px me-3'
-            />
-            Sign in with Google
-          </a>
-          {/* end::Google link */}
-        </div>
-        {/* end::Col */}
 
-        {/* begin::Col */}
-        <div className='col-md-6'>
-          {/* begin::Google link */}
-          <a
-            href='#'
-            className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100'
-          >
-            <img
-              alt='Logo'
-              src={toAbsoluteUrl('/media/svg/brand-logos/apple-black.svg')}
-              className='theme-light-show h-15px me-3'
-            />
-            <img
-              alt='Logo'
-              src={toAbsoluteUrl('/media/svg/brand-logos/apple-black-dark.svg')}
-              className='theme-dark-show h-15px me-3'
-            />
-            Sign in with Apple
-          </a>
-          {/* end::Google link */}
-        </div>
-        {/* end::Col */}
-      </div>
       {/* end::Login options */}
 
       {/* begin::Separator */}
-      <div className='separator separator-content my-14'>
-        <span className='w-125px text-gray-500 fw-semibold fs-7'>Or with email</span>
-      </div>
+
       {/* end::Separator */}
 
-      {formik.status ? (
+      {/* {formik.status ? (
         <div className='mb-lg-15 alert alert-danger'>
           <div className='alert-text font-weight-bold'>{formik.status}</div>
         </div>
@@ -131,11 +88,11 @@ export function Login() {
             continue.
           </div>
         </div>
-      )}
+      )} */}
 
       {/* begin::Form group */}
       <div className='fv-row mb-8'>
-        <label className='form-label fs-6 fw-bolder text-dark'>Email</label>
+        <label className='form-label fs-6 fw-bolder text-dark'>Correo</label>
         <input
           placeholder='Email'
           {...formik.getFieldProps('email')}
@@ -160,7 +117,7 @@ export function Login() {
 
       {/* begin::Form group */}
       <div className='fv-row mb-3'>
-        <label className='form-label fw-bolder text-dark fs-6 mb-0'>Password</label>
+        <label className='form-label fw-bolder text-dark fs-6 mb-0'>Contraseña</label>
         <input
           type='password'
           autoComplete='off'
@@ -217,10 +174,57 @@ export function Login() {
       {/* end::Action */}
 
       <div className='text-gray-500 text-center fw-semibold fs-6'>
-        Not a Member yet?{' '}
+        ¿No tiene una cuenta?{' '}
         <Link to='/auth/registration' className='link-primary'>
-          Sign up
+          Registrate
         </Link>
+      </div>
+
+      <div className='separator separator-content my-14'>
+        <span className='w-125px text-gray-500 fw-semibold fs-7'>O</span>
+      </div>
+
+      <div className='row g-3 mb-9'>
+        {/* begin::Col */}
+        <div className='col-md-6'>
+          {/* begin::Google link */}
+          <a
+            href='#'
+            className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100'
+          >
+            <img
+              alt='Logo'
+              src={toAbsoluteUrl('/media/svg/brand-logos/google-icon.svg')}
+              className='h-15px me-3'
+            />
+            Sign in with Google
+          </a>
+          {/* end::Google link */}
+        </div>
+        {/* end::Col */}
+
+        {/* begin::Col */}
+        <div className='col-md-6'>
+          {/* begin::Google link */}
+          <a
+            href='#'
+            className='btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100'
+          >
+            <img
+              alt='Logo'
+              src={toAbsoluteUrl('/media/svg/brand-logos/apple-black.svg')}
+              className='theme-light-show h-15px me-3'
+            />
+            <img
+              alt='Logo'
+              src={toAbsoluteUrl('/media/svg/brand-logos/apple-black-dark.svg')}
+              className='theme-dark-show h-15px me-3'
+            />
+            Sign in with Apple
+          </a>
+          {/* end::Google link */}
+        </div>
+        {/* end::Col */}
       </div>
     </form>
   )
